31e89ad3550923bce8106079dffc96f3ee7c1d27
Nic Palmer <Nicholasjamespalmer@googlemail.com>
2020-11-09 08:56:08 +0000

    Initial Commit
    First commit, be wary of bugs

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	ansibler "github.com/apenella/go-ansible"
+	"github.com/gin-gonic/gin"
+	"github.com/thedevsaddam/gojsonq"
+	"net/http"
+)
+
+type Alert struct {
+	ID       string `json:"id" binding:"required"`
+	Hostname string `json:"hostname" binding:"required"`
+	RuleName 	 string `json:"rulename" binding:"required"`
+	Inventory 	 string `json:"inventory" binding:"required"`
+}
+
+
+func main() {
+	router := gin.Default()
+	router.POST("/somePost", posting)
+	router.Run(":8080")
+
+}
+
+func posting(c *gin.Context){
+	var json Alert
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	ansible(json)
+	c.JSON(http.StatusOK, gin.H{"status": "success"})
+}
+
+func ansible(json Alert)  {
+	rulereturn := rulelookup(json)
+	playbookname := fmt.Sprintf("%s", rulereturn)
+	ansiblePlaybookConnectionOptions := &ansibler.AnsiblePlaybookConnectionOptions{
+		Connection: "local",
+	}
+
+	ansiblePlaybookOptions := &ansibler.AnsiblePlaybookOptions{
+		Inventory: json.Inventory+",",
+		ExtraVars: map[string]interface{}{
+			"example": "example",
+			"alertName": json.RuleName,
+			"hostname": json.Hostname,
+		},
+	}
+	playbook := &ansibler.AnsiblePlaybookCmd{
+		Playbook:          playbookname,
+		ConnectionOptions: ansiblePlaybookConnectionOptions,
+		Options:           ansiblePlaybookOptions,
+		ExecPrefix:        "ElastiSOAR",
+	}
+	fmt.Println(playbook.Run())
+
+}
+
+func rulelookup(json Alert) (interface{}){
+	jq := gojsonq.New().File("./alerts.json")
+	res := jq.Find(json.RuleName)
+	return res
+}
